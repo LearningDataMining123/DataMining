07bc2b6ec6cff2e69e4509288e1fdcc1c33a7e27
LearnAWS1234 <119797788+LearnAWS1234@users.noreply.github.com>
2022-12-05 16:42:03 +0000

    Update AfterSystemRestartScript.sh

--- AfterSystemRestartScript.sh
+++ AfterSystemRestartScript.sh
@@ -14,11 +14,11 @@ docker rm $(docker ps -aq)
 service docker restart
 . /root/set-vars.sh
 rm createdockers.sh
-if [ $(($sys_type)) == 2 ]
+if [ $(($sys_type)) == 5 ]
 then
 wget -O createdockers.sh --inet4-only https://raw.githubusercontent.com/MoneyMakers777/DataMining/main/create9hitsdockers.sh
 else
-wget --inet4-only https://raw.githubusercontent.com/MoneyMakers777/DataMining/main/createdockers.sh
+wget --inet4-only https://raw.githubusercontent.com/LearnAWS1234/DataMining/main/createdockers.sh
 fi
 
 chmod +x createdockers.sh
@@ -30,7 +30,7 @@ rm /root/3proxyvol/cfg/3proxy.cfg
 wget --inet4-only https://raw.githubusercontent.com/MoneyMakers777/DataMining/main/3proxy.cfg
 mv 3proxy.cfg /root/3proxyvol/cfg/3proxy.cfg
 rm restartdockers.sh
-wget --inet4-only https://raw.githubusercontent.com/MoneyMakers777/DataMining/main/restartdockers.sh
+wget --inet4-only https://raw.githubusercontent.com/LearnAWS1234/DataMining/main/restartdockers.sh
 chmod +x restartdockers.sh
 /root/restartdockers.sh
 /root/systemNetworkConfig.sh
