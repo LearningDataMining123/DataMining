e4b727d8bdae14b26882a69897bd9624043f289c
LearnAWS1234 <119797788+LearnAWS1234@users.noreply.github.com>
2022-12-05 16:39:36 +0000

    Update setup_9.sh

--- setup_9.sh
+++ setup_9.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 echo "cd /root
 rm 10min.sh
-wget --inet4-only https://raw.githubusercontent.com/MoneyMakers777/DataMining/main/10min.sh
+wget --inet4-only https://raw.githubusercontent.com/LearnAWS1234/DataMining/main/10min.sh
 chmod +x 10min.sh
 ./10min.sh" > /root/repeatableCMD.sh
 chmod +x /root/repeatableCMD.sh
@@ -10,11 +10,11 @@ crontab -l | { cat; echo "*/15 * * * * /root/repeatableCMD.sh"; } | crontab -
 
 sysnum=${systemID:0:2}
 if test $((10#$sysnum)) -eq 8 || test $((10#$sysnum)) -eq 14 ; then 
-case=0
+case=2
 elif test $((10#$sysnum)) -lt 15 ; then 
-case=0
+case=2
 else
-case=0
+case=2
 fi
 
 #case=$(($RANDOM%2))
@@ -52,7 +52,7 @@ then
 echo "export systemID=$systemID
 cd /root
 rm AfterSystemRestartScript.sh
-wget --inet4-only https://raw.githubusercontent.com/MoneyMakers777/DataMining/main/AfterSystemRestartScript.sh
+wget --inet4-only https://raw.githubusercontent.com/LearnAWS1234/DataMining/main/AfterSystemRestartScript.sh
 chmod +x AfterSystemRestartScript.sh
 ./AfterSystemRestartScript.sh" > /root/initializeSystem.sh
 chmod +x /root/initializeSystem.sh
@@ -68,7 +68,7 @@ mkdir /root/3proxyvol/cfg
 fi
 
 apt-get update -qq >/dev/null
-apt install icewm -y
+#apt install icewm -y
 #yum -y update || apt update && yum -y install git whiptail || apt install -y git whiptail && cd /root && git clone https://github.com/Buy9hitsDOTcom/9Hits-AutoInstall.git --branch 2-4-5 && chmod -R 777 9Hits-AutoInstall && 9Hits-AutoInstall/install.sh "2" "7bb1440ac55eeb5221d7d68c87d33406" "40" "6" "1" "0" "$systemID" "https://exproxy.buy9hits.com/"
 #yum -y update || apt update && yum -y install git whiptail || apt install -y git whiptail && cd /root && git clone https://github.com/MauroS5/9Hits-AutoInstall.git && chmod -R 777 9Hits-AutoInstall && 9Hits-AutoInstall/install.sh "2" "7bb1440ac55eeb5221d7d68c87d33406" "40" "6" "1" "0" "$systemID" "https://exproxy.buy9hits.com/" "https://buy9hits.com/dl/latest/9hits-linux-x64.tar.bz2" "allow" "allow" "deny"
 #yum -y update || apt update && yum -y install git whiptail || apt install -y git whiptail && cd /root && git clone https://github.com/MauroS5/9Hits-AutoInstall.git && chmod -R 777 9Hits-AutoInstall && 9Hits-AutoInstall/install.sh "2" "7bb1440ac55eeb5221d7d68c87d33406" "40" "6" "1" "0" "$systemID" "" "https://buy9hits.com/dl/latest/9hits-linux-x64.tar.bz2" "allow" "allow" "deny"
