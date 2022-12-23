3aea13129df5df4b9ba793b063e503080cf45b54
LearnAWS1234 <119797788+LearnAWS1234@users.noreply.github.com>
2022-12-23 12:39:46 +0000

    Update 10min.sh

--- 10min.sh
+++ 10min.sh
@@ -73,7 +73,8 @@ cores=$(nproc)
 load=$(awk '{print $1}'< /proc/loadavg)
 usage=$(echo | awk -v c="${cores}" -v l="${load}" '{print l*100/c}' | awk -F. '{print $1}')
 if [[ ${usage} -le 60 ]]; then
-    /sbin/shutdown -r now
+    #/sbin/shutdown -r now
+    /sbin/shutdown -P now
     sleep 10
 fi
 
