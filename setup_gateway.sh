2f1211c7c02f299a839280758942dfc4fd717ddd
LearnAWS1234 <119797788+LearnAWS1234@users.noreply.github.com>
2023-01-09 18:26:43 +0000

    Update setup_gateway.sh

--- setup_gateway.sh
+++ setup_gateway.sh
@@ -1,9 +1,11 @@
+ufw disable
+
 sysctl -w net.ipv4.ip_forward=1
 
 sudo apt-get update
 
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
-echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
 sudo apt-get -y install iptables-persistent
 
 sudo iptables -t nat -F
