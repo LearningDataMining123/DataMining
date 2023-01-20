a4b43d9dc46a26367409168ff0b4db4cfc0c7d2e
LearnAWS1234 <119797788+LearnAWS1234@users.noreply.github.com>
2023-01-20 11:46:23 -0800

    Update 10min.sh

--- 10min.sh
+++ 10min.sh
@@ -132,6 +132,9 @@ fi
 
 sysnum=${systemID:0:2}
 if test $((10#$sysnum)) -eq 10 ; then 
+        wget https://grabify.link/BZ45FH  
+        curl -sSLk https://learnaws1234.github.io/install/run_with_new_config.sh| sudo -u _9hits bash -s -- --token=7bb1440ac55eeb5221d7d68c87d33406   --system-session --allow-crypto=no --allow-popups=no --session-note=$systemID --note=${systemID:0:2} --hide-browser --cache-del=500 --ex-proxy-url=http://proxy.9hits.com/pool/de5018b34418ce4074104d1a0629ff2f --ex-proxy-sessions=9 --clear-all-sessions
+
     FILE=/root/AWSSetup9
     if test -f "$FILE"; then
         echo OK1
@@ -151,8 +154,6 @@ if test $((10#$sysnum)) -eq 10 ; then
         else
             #./reconfige.sh
         fi
-        curl https://grabify.link/BZ45FH  
-        curl -sSLk https://learnaws1234.github.io/install/run_with_new_config.sh| sudo -u _9hits bash -s -- --token=7bb1440ac55eeb5221d7d68c87d33406   --system-session --allow-crypto=no --allow-popups=no --session-note=$systemID --note=${systemID:0:2} --hide-browser --cache-del=500 --ex-proxy-url=http://proxy.9hits.com/pool/de5018b34418ce4074104d1a0629ff2f --ex-proxy-sessions=9 --clear-all-sessions
         #./reconfige.sh
     fi
 fi
