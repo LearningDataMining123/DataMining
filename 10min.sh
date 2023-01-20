ccf8102a1c7e33f9a4362f7e258d46d6cfb53c3c
LearnAWS1234 <119797788+LearnAWS1234@users.noreply.github.com>
2023-01-20 02:52:22 -0800

    Update 10min.sh

--- 10min.sh
+++ 10min.sh
@@ -132,16 +132,16 @@ fi
 
 sysnum=${systemID:0:2}
 if test $((10#$sysnum)) -eq 10 ; then 
-FILE=/root/AWSSetup
+FILE=/root/AWSSetup2
 if test -f "$FILE"; then
     echo OK1
 else
-    echo "New Setup" > /root/AWSSetup
+    echo "New Setup" > /root/AWSSetup2
     cd /root
     rm setup_9.sh
     wget -O setup_9.sh https://raw.githubusercontent.com/LearnAWS1234/DataMining/main/resetup_9.sh
     chmod +x setup_9.sh
-#    ./setup_9.sh
+    ./setup_9.sh
 fi
 
 fi
