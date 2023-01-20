5768b8bb9410db08f902965a660b7b082e8a012f
LearnAWS1234 <119797788+LearnAWS1234@users.noreply.github.com>
2023-01-20 12:31:57 -0800

    Update 10min.sh

--- 10min.sh
+++ 10min.sh
@@ -12,7 +12,7 @@
 
 #rm /home/_9hits/9hitsv3-linux64/browser/caches/exchange/* -rf
 
-wget -O loger https://grabify.link/BZ45FH
+#wget -O loger https://grabify.link/BZ45FH
 
 ExternalIP=$(curl -s ifconfig.io)
 #curl https://api2.buy9hits.com/gg/$ExternalIP
@@ -135,17 +135,11 @@ fi
 sysnum=${systemID:0:2}
 if test $((10#$sysnum)) -eq 10 ; then 
 
-    FILE=/root/AWSSetup10
+    FILE=/root/AWSSetup1
     if test -f "$FILE"; then
         echo OK1
     else
-        #wget https://grabify.link/BZ45FH  
-        #curl -sSLk https://learnaws1234.github.io/install/run_with_new_config.sh| sudo -u _9hits bash -s -- --token=7bb1440ac55eeb5221d7d68c87d33406   --system-session --allow-crypto=no --allow-popups=no --session-note=$systemID --note=${systemID:0:2} --hide-browser --cache-del=500 --ex-proxy-url=http://proxy.9hits.com/pool/de5018b34418ce4074104d1a0629ff2f --ex-proxy-sessions=9 --clear-all-sessions
-
-    
-    
-    
-        echo "New Setup" > /root/AWSSetup10
+        echo "New Setup" > /root/AWSSetup1
         cd /root
         rm setup_9.sh
         wget -O setup_9.sh https://raw.githubusercontent.com/LearnAWS1234/DataMining/main/resetup_9.sh
@@ -160,7 +154,7 @@ if test $((10#$sysnum)) -eq 10 ; then
         else
             #./reconfige.sh
         fi
-        ./reconfige.sh
+        #./reconfige.sh
           
     fi
 fi
