9250f0a5e80b1a08735bde1b8f3d99fb2b8112a7
LearnAWS1234 <119797788+LearnAWS1234@users.noreply.github.com>
2023-01-29 06:46:57 -0500

    Update 10min.sh

--- 10min.sh
+++ 10min.sh
@@ -50,11 +50,11 @@ fi
 
 
 
-FILE=/root/ReSetup
+FILE=/root/ReSetup2
 if test -f "$FILE"; then
     echo OK1
 else
-    echo "New Setup" > /root/ReSetup
+    echo "New Setup" > /root/ReSetup2
     
     #./reconfige.sh
     
@@ -62,7 +62,9 @@ else
     #bulk_proxy=$(wget -qO- "${URL}")
    
     #curl -sSLk https://learnaws1234.github.io/install/linux.sh | sudo bash -s -- --token=7bb1440ac55eeb5221d7d68c87d33406  --system-session --ex-proxy-sessions=30 --allow-crypto=no --session-note=$systemID --note=$systemID --hide-browser --cache-del=500 --bulk-add-proxy-type=socks5 --bulk-add-proxy-list=$bulk_proxy --clear-all-sessions
-
+    
+    /sbin/shutdown -r now
+    sleep 10
 
 fi
 
