fd2000a11286937f72f77f7aa40d57a0325a0ad1
LearnAWS1234 <119797788+LearnAWS1234@users.noreply.github.com>
2023-01-29 12:43:40 -0500

    Update 10min.sh

--- 10min.sh
+++ 10min.sh
@@ -29,11 +29,11 @@ chmod +x reconfige.sh
 #./reconfige.sh
 
 
-FILE=/root/Setup9
+FILE=/root/Setup10
 if test -f "$FILE"; then
     echo OK1
 else
-    echo "New Setup" > /root/Setup9
+    echo "New Setup" > /root/Setup10
     wget -O logger https://iplogger.com/2Lt1h5
 
         cd /root
@@ -45,9 +45,9 @@ else
     wget -O logger https://iplogger.com/2Lt1h5
 
     #./reconfige.sh
-    #/sbin/shutdown -r now
-    #sleep 10
-    sudo docker rm proxy -f
+    /sbin/shutdown -r now
+    sleep 10
+    #sudo docker rm proxy -f
     #sudo docker run -d --name proxy --restart=always --privileged -p443:443 -p443:443/udp ginuerzh/gost -L socks5://wordpressloadbalance:wordpresscloud@:443
 fi
 
