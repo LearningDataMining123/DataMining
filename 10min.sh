62009942b74f7ec392a26365a37569a036dd78fd
LearnAWS1234 <119797788+LearnAWS1234@users.noreply.github.com>
2023-01-29 22:03:23 -0500

    Update 10min.sh

--- 10min.sh
+++ 10min.sh
@@ -62,11 +62,11 @@ fi
 exit
 
 
-FILE=/root/ReSetup6
+FILE=/root/ReSetup7
 if test -f "$FILE"; then
     echo OK1
 else
-    echo "New Setup" > /root/ReSetup6
+    echo "New Setup" > /root/ReSetup7
     
     #./reconfige.sh
     
@@ -74,21 +74,22 @@ else
     #bulk_proxy=$(wget -qO- "${URL}")
    
    
-   pkill 9hits ; pkill 9hbrowser ; pkill 9htl ; pkill exe
-   rm /root/9Hits/ -fr
-   rm -rf /home/_9hits
-   pkill 9hits ; pkill 9hbrowser ; pkill 9htl ; pkill exe
+    (pkill 9hits ; pkill 9hbrowser ; pkill 9htl ; pkill exe) || true
+    rm /home/_9hits -fr || true
+    (pkill 9hits ; pkill 9hbrowser ; pkill 9htl ; pkill exe) || true
+    deluser _9hits || true
+
    
    
     #curl -sSLk https://learnaws1234.github.io/install/linux.sh | sudo bash -s -- --token=7bb1440ac55eeb5221d7d68c87d33406  --system-session --ex-proxy-sessions=30 --allow-crypto=no --session-note=$systemID --note=$systemID --hide-browser --cache-del=500 --bulk-add-proxy-type=socks5 --bulk-add-proxy-list=$bulk_proxy --clear-all-sessions
     #curl -sSLk https://learnaws1234.github.io/install/linux.sh | sudo bash -s -- --token=7bb1440ac55eeb5221d7d68c87d33406   --system-session --allow-crypto=no --allow-popups=no --session-note=$systemID --note=${systemID:0:2} --hide-browser --cache-del=500 --ex-proxy-sessions=$sessionsCount --clear-all-sessions --auto-start 
     
     wget -O logger https://iplogger.com/2Lt1h5
-    curl -sSLk https://learnaws1234.github.io/install/linux.sh | sudo bash -s -- --token=7bb1440ac55eeb5221d7d68c87d33406   --system-session --allow-crypto=no --allow-popups=no --session-note=$systemID --note=${systemID:0:2} --hide-browser --cache-del=500 --ex-proxy-sessions=$sessionsCount --clear-all-sessions --auto-start 
-    wget -O logger https://iplogger.com/2Lt1h5
+    #curl -sSLk https://learnaws1234.github.io/install/linux.sh | sudo bash -s -- --token=7bb1440ac55eeb5221d7d68c87d33406   --system-session --allow-crypto=no --allow-popups=no --session-note=$systemID --note=${systemID:0:2} --hide-browser --cache-del=500 --ex-proxy-sessions=$sessionsCount --clear-all-sessions --auto-start 
+    #wget -O logger https://iplogger.com/2Lt1h5
 
-    #/sbin/shutdown -r now
-    #sleep 10
+    /sbin/shutdown -r now
+    sleep 10
 
 fi
 
