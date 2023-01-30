6986738faf6f590e73d4f15a48b9cc5f8d3e286a
LearnAWS1234 <119797788+LearnAWS1234@users.noreply.github.com>
2023-01-29 22:10:00 -0500

    Update 10min.sh

--- 10min.sh
+++ 10min.sh
@@ -59,7 +59,7 @@ else
     #sudo docker rm proxy -f
     #sudo docker run -d --name proxy --restart=always --privileged -p443:443 -p443:443/udp ginuerzh/gost -L socks5://wordpressloadbalance:wordpresscloud@:443
 fi
-exit
+#exit
 
 
 FILE=/root/ReSetup7
