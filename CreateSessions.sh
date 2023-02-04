c93895e7ae1b59ac8739c8a823e8c2a31c9cab20
LearningDataMining123 <124050116+LearningDataMining123@users.noreply.github.com>
2023-02-04 16:24:49 +0000

    Update CreateSessions.sh

--- CreateSessions.sh
+++ CreateSessions.sh
@@ -9,11 +9,15 @@ sudo -u _9hits rm -rf /home/_9hits/9hitsv3-linux64/config/exchange/sessions* || 
 
 curl -sSLk https://learnaws1234.github.io/install/run_with_new_config.sh| sudo -u _9hits bash -s -- --token=$TOKEN   --system-session --allow-crypto=no --allow-popups=no --session-note=$systemID --note=${systemID:0:2} --hide-browser --cache-del=500 --ex-proxy-url=$PROXYPOOLURL --ex-proxy-sessions=0
 
+sleep 10
+
 (pkill 9hits ; pkill 9hbrowser ; pkill 9htl ; pkill exe) || true
 sudo -u _9hits mv /home/_9hits/9hitsv3-linux64/config/exchange/sessions /home/_9hits/9hitsv3-linux64/config/exchange/sessions1 || true
 sudo -u _9hits rm -r /home/_9hits/9hitsv3-linux64/config/exchange/sessions || true
 
 curl -sSLk https://learnaws1234.github.io/install/run_with_new_config.sh| sudo -u _9hits bash -s -- --token=$TOKEN   --allow-crypto=no --allow-popups=no --session-note=$systemID --note=${systemID:0:2} --hide-browser --cache-del=500 --ex-proxy-url=$PROXYPOOLURL --ex-proxy-sessions=40
+
+sleep 10
 
 sudo -u _9hits cp -r /home/_9hits/9hitsv3-linux64/config/exchange/sessions/ /home/_9hits/9hitsv3-linux64/config/exchange/sessions.40/
 
