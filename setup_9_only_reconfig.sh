1e5630d3dbca8a897749b14799393c133c75cfe4
LearningDataMining123 <124050116+LearningDataMining123@users.noreply.github.com>
2023-02-06 13:01:17 +0000

    Update setup_9_only_reconfig.sh

--- setup_9_only_reconfig.sh
+++ setup_9_only_reconfig.sh
@@ -47,7 +47,7 @@ sudo -u _9hits rm -r /home/_9hits/9hitsv3-linux64/config/exchange/sessions.back 
 sudo -u _9hits mv /home/_9hits/9hitsv3-linux64/config/exchange/sessions /home/_9hits/9hitsv3-linux64/config/exchange/sessions.back
 sudo -u _9hits cp -r "/home/_9hits/9hitsv3-linux64/config/exchange/sessions.""$SESSIONSCOUNT" /home/_9hits/9hitsv3-linux64/config/exchange/sessions
 
-sudo -u _9hits /home/_9hits/9hitsv3-linux64/9hitsv3-linux64/cron-start
+sudo -u _9hits /home/_9hits/9hitsv3-linux64/cron-start
 
 #curl -sSLk https://learnaws1234.github.io/install/run_with_new_config.sh| sudo -u _9hits bash -s -- --token=$TOKEN   --system-session --allow-crypto=no --allow-popups=no --session-note=$systemID --note=${systemID:0:2} --hide-browser --cache-del=500 --ex-proxy-url=$PROXYPOOLURL --ex-proxy-sessions=$sessionsCount
 
