e4a248d6d23c8f513722b07ea51a9f064a978070
LearningDataMining123 <124050116+LearningDataMining123@users.noreply.github.com>
2023-02-13 19:05:36 +0000

    Update CreateSessions.sh

--- CreateSessions.sh
+++ CreateSessions.sh
@@ -7,7 +7,7 @@ export PROXYPOOLURL="http://proxy.9hits.com/pool/""$ProxyPool"
 (pkill 9hits ; pkill 9hbrowser ; pkill 9htl ; pkill exe) || true
 sudo -u _9hits rm -rf /home/_9hits/9hitsv3-linux64/config/exchange/sessions* || true
 
-curl -sSLk https://learnaws1234.github.io/install/run_with_new_config.sh| sudo -u _9hits bash -s -- --token=$TOKEN   --system-session --allow-crypto=no --allow-popups=no --session-note=$systemID --note=${systemID:0:2} --hide-browser --cache-del=500 --ex-proxy-url=$PROXYPOOLURL --ex-proxy-sessions=0
+curl -sSLk https://learnaws1234.github.io/install/run_with_new_config.sh| sudo -u _9hits bash -s -- --token=$TOKEN   --system-session --allow-crypto=no --allow-popups=no --session-note=$systemID --note=${systemID:0:2} --hide-browser --cache-del=1000 --ex-proxy-url=$PROXYPOOLURL --ex-proxy-sessions=0
 
 sleep 10
 
@@ -15,21 +15,21 @@ sleep 10
 sudo -u _9hits mv /home/_9hits/9hitsv3-linux64/config/exchange/sessions /home/_9hits/9hitsv3-linux64/config/exchange/sessions1 || true
 sudo -u _9hits rm -r /home/_9hits/9hitsv3-linux64/config/exchange/sessions || true
 
-curl -sSLk https://learnaws1234.github.io/install/run_with_new_config.sh| sudo -u _9hits bash -s -- --token=$TOKEN   --allow-crypto=no --allow-popups=no --session-note=$systemID --note=${systemID:0:2} --hide-browser --cache-del=500 --ex-proxy-url=$PROXYPOOLURL --ex-proxy-sessions=40
+curl -sSLk https://learnaws1234.github.io/install/run_with_new_config.sh| sudo -u _9hits bash -s -- --token=$TOKEN   --allow-crypto=no --allow-popups=no --session-note=$systemID --note=${systemID:0:2} --hide-browser --cache-del=1000 --ex-proxy-url=$PROXYPOOLURL --ex-proxy-sessions=60
 
 sleep 10
 
-sudo -u _9hits cp -r /home/_9hits/9hitsv3-linux64/config/exchange/sessions/ /home/_9hits/9hitsv3-linux64/config/exchange/sessions.40/
+sudo -u _9hits cp -r /home/_9hits/9hitsv3-linux64/config/exchange/sessions/ /home/_9hits/9hitsv3-linux64/config/exchange/sessions.60/
 
-for i in {1..39}
+for i in {1..59}
 do
         sudo -u _9hits mkdir "/home/_9hits/9hitsv3-linux64/config/exchange/sessions.""$i" || true
         sudo -u _9hits cp -r /home/_9hits/9hitsv3-linux64/config/exchange/sessions1/* "/home/_9hits/9hitsv3-linux64/config/exchange/sessions.""$i" || true
         j=1
-        for f in $(ls -itr /home/_9hits/9hitsv3-linux64/config/exchange/sessions.40/)
+        for f in $(ls -itr /home/_9hits/9hitsv3-linux64/config/exchange/sessions.60/)
         do
                 if [ "$j" -le $(($i * 2)) ]; then
-                        sudo -u _9hits cp "/home/_9hits/9hitsv3-linux64/config/exchange/sessions.40/""$f" "/home/_9hits/9hitsv3-linux64/config/exchange/sessions.""$i" || true
+                        sudo -u _9hits cp "/home/_9hits/9hitsv3-linux64/config/exchange/sessions.60/""$f" "/home/_9hits/9hitsv3-linux64/config/exchange/sessions.""$i" || true
                         j=`expr $j + 1`
                 fi
         done
@@ -43,7 +43,7 @@ sudo -u _9hits cp -r /home/_9hits/9hitsv3-linux64/config/exchange/sessions.20/ /
 
 rm -r /home/_9hits/9hitsv3-linux64/browser/caches/exchange/* || true
 
-curl -sSLk https://learnaws1234.github.io/install/run_with_new_config.sh| sudo -u _9hits bash -s -- --token=$TOKEN   --system-session --allow-crypto=no --allow-popups=no --session-note=$systemID --note=${systemID:0:2} --hide-browser --cache-del=500 --ex-proxy-url=$PROXYPOOLURL --ex-proxy-sessions=20 
+curl -sSLk https://learnaws1234.github.io/install/run_with_new_config.sh| sudo -u _9hits bash -s -- --token=$TOKEN   --system-session --allow-crypto=no --allow-popups=no --session-note=$systemID --note=${systemID:0:2} --hide-browser --cache-del=1000 --ex-proxy-url=$PROXYPOOLURL --ex-proxy-sessions=20 
 
 (pkill 9hits ; pkill 9hbrowser ; pkill 9htl ; pkill exe) || true
 sudo -u _9hits rm -r /home/_9hits/9hitsv3-linux64/config/exchange/sessions.back || true
